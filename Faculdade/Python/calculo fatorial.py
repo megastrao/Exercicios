b11120de391c01d464b850b9fa7ef9630a57d7d4
Gabriel Carvalho <gabrielbonadio@hotmail.com>
2023-02-19 05:35:25 -0300

    18/02/2023

--- Faculdade/Python/calculo fatorial.py
+++ Faculdade/Python/calculo fatorial.py
@@ -1,20 +1,19 @@
-number = int(inpút('Me de um numero!'))
+number = int(input('Me de um numero!'))
 
 #Estrategia 1
 def fatorial_iterativo(n):
     f=1
-    for i in range(1,)
+    for i in range(1,n+1):
+        f=f*i
+    return f
 
 
 #Estrategia 2
-def fatorial_recursivo(num):
+def fatorial_recursivo(n):
+    if ((n==0)or(n==1)):
+        return 1
+    return n*fatorial_recursivo(n-1)
+    
 
-    if number>1:
-        função
-    else
-        fatorial = 1
-
-
-
-print(f'O fatorial de {number} é {fatorial_iterativo}')
-print(f'O fatorial de {number} é {fatorial_recursivo}')
+print(f'O fatorial de {number} é {fatorial_iterativo(number)}')
+print(f'O fatorial de {number} é {fatorial_recursivo(number)}')
